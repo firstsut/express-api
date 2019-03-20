332bc15eeec71560469daa583d7e7d999fd3c54a
Watcharaphong Ketcharoen <first_jks@hotmail.com>
2019-03-20 19:23:37 +0700

    Add Route,Service,Model,validate for crud genre

---
+++ index.js
@@ -0,0 +1,58 @@
+const log = require('./middleware/logger');
+const helmet = require('helmet');
+const config = require('config');
+const fs = require('fs');
+const morgan = require('morgan');
+const path = require('path');
+const Authenticate = require('./middleware/authentication');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const courses_route = require('./routes/courses');
+const genres_route = require('./routes/genres');
+
+const logDirectory = path.join(__dirname, 'logs');
+
+log(`Application Name : ${config.get('name')}`);
+
+app.use(helmet()); // middleware for helps you secure
+app.use(express.json()); // req.body 
+app.use(express.urlencoded({extended:true})); // form with input fields (key=value&key1=value1)
+app.use(express.static('public')); //static public path
+app.use(Authenticate); //Custom middleware
+
+// log all requests to access.log,error.log
+app.use(morgan('dev'));
+app.use(morgan('combined', {
+    skip: function (req, res) { return res.statusCode >= 400 },
+    stream: fs.createWriteStream(logDirectory+'/access.log', { flags: 'a' })
+}));
+app.use(morgan('combined', {
+    skip: function (req, res) { return res.statusCode < 400 },
+    stream: fs.createWriteStream(logDirectory+'/error.log', { flags: 'a' })
+}));
+
+//Connect mongoDB
+mongoose.connect('mongodb://localhost:27017/api',
+    { 
+        useNewUrlParser: true,
+        useFindAndModify : false,
+        useCreateIndex : true
+    })
+    .then(()=>{      
+        log('Connected to MongoDB...');
+    }).catch(err=>{
+        log('Could not connect to MongoDB...',err);
+    })
+
+//use router
+app.get('/',(req,res)=>{  res.send('Express RESTful API...'); });
+app.use('/api/courses',courses_route);
+app.use('/api/genres',genres_route);
+
+
+//Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    log(`Listening on port : ${PORT}...`);
+});
---
+++ routes/genres.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const route = express.Router();
+const GenreService = require('../services/genre_service');
+const {isValid} = require('../้helper/checkObjectId');
+const ErrorResponse = require('../้helper/errorResponse');
+
+route.get('/',async (req,res)=>{
+    const genres = await GenreService.list();  
+    res.send(genres);
+});
+
+route.get('/:id',async (req,res)=>{ 
+    const {id} = req.params;  
+    if(!isValid(id)){
+        return res.status(400).json({
+            error : ErrorResponse("Genre ID invalid")
+        });
+    }
+    const genre = await GenreService.findById(id);  
+    res.send(genre);
+});
+
+route.post('/',async (req,res)=>{
+    try{
+        const {error} = GenreService.validate(req.body);
+        if(error){
+            return res.status(400).json({
+                error : ErrorResponse(error.details)
+            });      
+        } 
+        const genre = await GenreService.save(req.body);
+        return res.status(201).json(genre);
+    }catch(err){
+        return res.status(500).json({
+            error : ErrorResponse(err)
+        })    
+    }
+});
+
+route.put('/:id',async (req,res)=>{
+    const {id} = req.params;
+    if(!isValid(id)){
+        return res.status(400).json({
+            error :  ErrorResponse("Genre ID invalid")
+        });
+    }
+    try{
+        const {error} = GenreService.validate(req.body);
+        if(error){
+            return res.status(400).json({
+                error : ErrorResponse(error.details)
+            });      
+        } 
+        const genre = await GenreService.update(id,req.body);
+        if(!genre){
+            return res.status(404).json({
+                error : ErrorResponse("Genre ID not found")
+            });
+        }
+        return res.json(genre);
+    }catch(err){
+        return res.status(500).json({
+            error : ErrorResponse(err)
+        })    
+    }
+});
+
+route.delete('/:id',async (req,res)=>{
+    const {id} = req.params;
+    if(!isValid(id)){
+        return res.status(400).json({
+            error :  ErrorResponse("Genre ID invalid")
+        });
+    }
+    try{
+        const genre = await GenreService.del(id);
+        if(!genre){
+            return res.status(404).json({
+                error : ErrorResponse("Genre ID not found")
+            });
+        }
+        return res.json(genre);
+    }catch(err){
+        return res.status(500).json({
+            error : ErrorResponse(err)
+        })   
+    }
+   
+});
+
+module.exports = route;
